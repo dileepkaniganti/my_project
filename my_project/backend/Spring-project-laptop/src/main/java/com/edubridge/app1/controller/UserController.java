c3014518b6e1512770b363c1b296d71869ec06ff
dileepkaniganti <dileepkaniganti234@gmail.com>
2024-01-30 15:40:20 +0530

    first commit

---
+++ my_project/backend/Spring-project-laptop/src/main/java/com/edubridge/app1/controller/UserController.java
@@ -0,0 +1,5 @@
+package com.edubridge.app1.controller;
+
+public class UserController {
+
+}
---
+++ my_project/frontend/project/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LaptopListComponent } from './components/laptop-list/laptop-list.component';
+import { LaptopCategoryMenuComponent } from './components/laptop-category-menu/laptop-category-menu.component';
+import { SearchComponent } from './components/search/search.component';
+import { LaptopDetailsComponent } from './components/laptop-details/laptop-details.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SigninComponent } from './components/signin/signin.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LaptopListComponent,
+    LaptopCategoryMenuComponent,
+    SearchComponent,
+    LaptopDetailsComponent,
+    CartDetailsComponent,
+    CartStatusComponent,
+    CheckoutComponent,
+    SignupComponent,
+    SigninComponent,
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
